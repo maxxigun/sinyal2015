2e46016e93978173f26e6ae97c07b8709c9ea831
maxxigun <bornova-s@hotmail.com>
2015-12-22 11:43:58 +0200

    Yeni Note.m Dosyası Oluşturuldu.

--- note.m
+++ note.m
@@ -1,5 +1,7 @@
+function [x,t] = note(f,vurus) %argumaný f ve vurus olan fonksiyon tanýmladýk
 
-function [a,b]=note (f,dur)% fonksiyonu tanýmlýyoruz.argumanlarý f ve dur 
-b=0:1/(10000000*f):dur; %zaman aralýgýný belirledik 0 dan dur'a kadar (100*f)artarak gidiyor
-a=sin(2*pi*f*b);  %sinüs'ü olustrukduk               
-plot(b,a);%plot ile grafigi cizdirdik.
+fs=8192;%örnekleme frekansý
+
+t = 0:1/fs:(vurus-1/fs); %t yi tanýmladik
+
+x = sin(2*pi*f*t); %sinüs olusturduk
