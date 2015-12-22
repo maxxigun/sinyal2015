3953f26de8c5911d5f9e24ec922bf1c521b3ce5e
maxxigun <bornova-s@hotmail.com>
2015-12-22 13:24:10 +0200

    Frek Düzenlenerek degiştirildi.

--- frek.m
+++ frek.m
@@ -1,5 +1,25 @@
-function [a]=frek(n,o) %fonksiyon olusturduk.girilecek degerler nota(n) ve oktav(o)
-notalar={'Do','C#','Re','Eb','Mi','Fa','F#','Sol','G#','La','Bb','Si'};     %notalarý diziye koyduk
-b=strcmp(notalar,n); %ustte girilen notayý dizilerde buluyor.alttakþ c=find(b,1) indeksini buluyor ve formulu ekliyor                                                    
-c=find(b,1); %girilen notanýn indeksini buluyor                   
-a=round(16.35*(2^o)*(2^((c-1)/12)));%sayý cok uzun cýktýgý icin round ile en yakin tam sayiya yuvarladik.
+  function [frekans]=frek(n,o)                                         %Fonksiyonlarý olustyurduk argumanlarý n(nota),o (oktav)
+notalar={'Do','C#','Re','Eb','Mi','Fa','F#','Sol','G#','La','Bb','Si'};     %Notalarý diziye aktardým.
+ n=strcmp(notalar,n);              %Fonksiyona gönderdiðimiz notayý dizi içerisinde aratýr ve buldugu notanýn indeksini 1 artýrýr
+ m=find(n,1);                                                                %n dizisinde deðerinin indeksini döndürür. 
+ 
+ 
+ 
+ 
+ if (nargin==0) 
+       disp ('Lütfen veri giriþi yapýnýz: ') 
+   else if (nargin==1) 
+         if(m==13) 
+             frekans=0; 
+        else 
+           frekans=floor(16.35*(2^4)*(2^((m-1)/12)));                       % Frekans fonksiyonu oluþturdum.  
+            
+         end 
+     else  
+         if (m==13)  
+            frekans=0; 
+         else frekans=floor(16.35*(2^o)*(2^((m-1)/12)));                 % Frekans formulu  
+        end 
+         
+     end 
+ end                                                                         %floor =sayýyý en yakýn deðere yuvarladýk yuvarlama kodu. 
