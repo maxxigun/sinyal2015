88106b4e5bd49fbd93583031686ca364fb89e32e
maxxigun <bornova-s@hotmail.com>
2015-12-29 12:06:37 +0200

    Frek Düzenlendi.

--- frek.m
+++ frek.m
@@ -1,25 +1,33 @@
-  function [frekans]=frek(n,o)                                         %Fonksiyonlarý olustyurduk argumanlarý n(nota),o (oktav)
-notalar={'Do','C#','Re','Eb','Mi','Fa','F#','Sol','G#','La','Bb','Si'};     %Notalarý diziye aktardým.
- n=strcmp(notalar,n);              %Fonksiyona gönderdiðimiz notayý dizi içerisinde aratýr ve buldugu notanýn indeksini 1 artýrýr
- m=find(n,1);                                                                %n dizisinde deðerinin indeksini döndürür. 
- 
- 
- 
- 
- if (nargin==0) 
-       disp ('Lütfen veri giriþi yapýnýz: ') 
-   else if (nargin==1) 
-         if(m==13) 
-             frekans=0; 
+function [frekans]=frek(n,o,odegeri)%Frekans Oluþturuldu.o(oktav),n(nota),odegeri(oktav degeri)
+
+notalar={'Do','Dod','Re','Mib','Mi','Fa','Fad','Sol','Sold','La','Sib','Si','Sus'}; %notalarý diziye aktardým.
+n=strcmp(notalar,n); %Ýndeksi 1 arttýroyr.
+m=find(n,1);% n deki 1 degerini buluyor.
+        if (nargin==0) %hiç bir deger girilmediyse aþagýdaki kodlarý calýstýrýyor.
+                 disp ('veri giriþi yap:');
+        elseif (nargin==1)%tek deger girdiyse aþagadaki kodlarý çalýþtýryor.
+                 if (m==13)% sus sa Frekans 0 olur.
+                    frekans=0;
+                else 
+                    frekans=16.35*(2^(4))*(2^((m-1)/12));%Frekansý hesaplar.
+                 end
+        elseif (nargin==2) %iki deger girilmediyse aþagýdaki kodlarý calýstýrýyor..
+                 if (m==13)%sus sa Frekans 0 olur.
+                    frekans=0;
+                 else
+                    frekans=16.35*(2^(o))*(2^((m-1)/12)); %Frekans hesaplar.
+                 end
         else 
-           frekans=floor(16.35*(2^4)*(2^((m-1)/12)));                       % Frekans fonksiyonu oluþturdum.  
-            
-         end 
-     else  
-         if (m==13)  
-            frekans=0; 
-         else frekans=floor(16.35*(2^o)*(2^((m-1)/12)));                 % Frekans formulu  
-        end 
-         
-     end 
- end                                                                         %floor =sayýyý en yakýn deðere yuvarladýk yuvarlama kodu. 
+                 if (m==13)%sus sa Frekans 0 olur
+                     frekans=0;
+                 else
+                     frekans=16.35*(2^(o+odegeri))*(2^((m-1)/12)); %Frekans hesaplar.
+                 end
+
+
+    end
+
+end 
+   
+
+
