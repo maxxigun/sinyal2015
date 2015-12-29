386b0e8bb4f40bcea825eb8e83cfdf00072271d0
maxxigun <bornova-s@hotmail.com>
2015-12-29 12:07:44 +0200

    Note Düzenlendi.

--- note.m
+++ note.m
@@ -1,36 +1,17 @@
-function [a,b] = note(f,vurus) %fonksiyonu tanimliyoruz.girilicek argumanlari tanýmladýk
-
-fs=8192;%örnekleme frekansý
-
-b = 0:1/fs:vurus; %zaman aralýgý
-
-
-
-
-
-    elemanS = length(b); %% Eleman sayýsý
-
-    
-
-    attack = linspace(0,1.5,(elemanS*2/8));
-
-    
-
-    decay = linspace(1.5-(0.5/(elemanS/8)),1,(elemanS/8));
-
-    
-
-    sustain = ones(1,(elemanS/2));
-
-    
-
-    relase = linspace(1-(1/(elemanS/8)),0,(elemanS/8));
-
-    
-
-    zarf = [attack, decay, sustain, relase];
-
-
-
-
-    a = zarf .* sin(2*pi*f*b + zarf); %% zarflý sinüs
+function[e,q]=note(frekans,vurus)%sinyali çizer.
+    fs=8192;%örneklenme tanýmladým.
+    q=0:1/fs:vurus-(1/fs); 
+       a=length(q);
+    hk=[1 0.8 0.4 0.1]; %harmonik katsayýlþarý dizeye atadým.
+        b=length(hk); %hk dizisinin boyut belirler.
+        e=zeros(1,a); %0 matrisi olusturur.
+        for i=1:b %1 den hk dizisi boyutuna kadar bir dizi olusturur.
+            e=e+(hk(i)*sin(2*pi*(i*frekans)*q)); % harmonikleri ekledik.
+        end
+A=linspace(0,1.5,a/4);%vektörleri olusturdum.
+B=linspace(1.5,1,a/8);%vektörleri olusturdum.
+C=linspace(1,1,a/2);%vektörleri olusturdum.
+D=linspace(1,0,a/8);%vektörleri olusturdum.
+ zarf=[A B C D];%üstteki vektçrleri diziye atýyoruz.
+ e=e.*zarf;% Zarflama iþlemi yapýyoruz.
+ 
